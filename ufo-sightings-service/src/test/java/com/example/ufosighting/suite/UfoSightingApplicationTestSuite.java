f0d16d630cc05eb7352db70e8f8bc73f18a528cc
Rajashekar <rajashekar.katla01@gmail.com>
2018-03-16 19:39:02 +0000

    test

--- ufo-sightings-service/src/test/java/com/example/ufosighting/suite/UfoSightingApplicationTestSuite.java
+++ ufo-sightings-service/src/test/java/com/example/ufosighting/suite/UfoSightingApplicationTestSuite.java
@@ -4,11 +4,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import com.example.ufosighting.UfoSightingApplicationTests;
+import com.example.ufosighting.controller.UfoSightingControllerTests;
 import com.example.ufosighting.integration.UfoSightingIntegrationTests;
 
 @RunWith(Suite.class)
-@SuiteClasses({ UfoSightingApplicationTests.class, UfoSightingIntegrationTests.class })
+@SuiteClasses({ UfoSightingControllerTests.class, UfoSightingIntegrationTests.class })
 public class UfoSightingApplicationTestSuite {
 
 }
